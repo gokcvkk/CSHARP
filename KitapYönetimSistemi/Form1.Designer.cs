e6c7d0ac94ce2b7b95d47adc0db90017ea37399e
GÖKCE ÇEVİK <119782405+gokcvkk@users.noreply.github.com>
2024-06-13 14:59:07 +0300

    Add files via upload

---
+++ KitapYönetimSistemi/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace BookManagementSystem
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.numYear = new System.Windows.Forms.NumericUpDown();
+            this.btnAddBook = new System.Windows.Forms.Button();
+            this.lstBooks = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numYear)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(92, 28);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(189, 22);
+            this.txtTitle.TabIndex = 0;
+            this.txtTitle.Text = "Kitap İsmi";
+            this.txtTitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(92, 90);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(189, 22);
+            this.txtAuthor.TabIndex = 1;
+            this.txtAuthor.Text = "Yazar İsmi";
+            this.txtAuthor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numYear
+            // 
+            this.numYear.Location = new System.Drawing.Point(92, 151);
+            this.numYear.Maximum = new decimal(new int[] {
+            2100,
+            0,
+            0,
+            0});
+            this.numYear.Minimum = new decimal(new int[] {
+            1500,
+            0,
+            0,
+            0});
+            this.numYear.Name = "numYear";
+            this.numYear.Size = new System.Drawing.Size(189, 22);
+            this.numYear.TabIndex = 2;
+            this.numYear.Value = new decimal(new int[] {
+            2024,
+            0,
+            0,
+            0});
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.Location = new System.Drawing.Point(151, 197);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(75, 23);
+            this.btnAddBook.TabIndex = 3;
+            this.btnAddBook.Text = "Kitap Ekle";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            // 
+            // lstBooks
+            // 
+            this.lstBooks.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lstBooks.FormattingEnabled = true;
+            this.lstBooks.ItemHeight = 16;
+            this.lstBooks.Location = new System.Drawing.Point(92, 257);
+            this.lstBooks.Name = "lstBooks";
+            this.lstBooks.Size = new System.Drawing.Size(189, 244);
+            this.lstBooks.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(372, 544);
+            this.Controls.Add(this.lstBooks);
+            this.Controls.Add(this.btnAddBook);
+            this.Controls.Add(this.numYear);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numYear)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.NumericUpDown numYear;
+        private System.Windows.Forms.Button btnAddBook;
+        private System.Windows.Forms.ListBox lstBooks;
+
+        // Tasarım kodundan sadece kontrol tanımlamalarını çıkardık, diğer kodları burada bırakarak tasarımı elle yapabilirsiniz.
+    }
+}
---
+++ KitapYönetimSistemi/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BookManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        private List<Book> library = new List<Book>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddBook_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text;
+            string author = txtAuthor.Text;
+            int year = (int)numYear.Value;
+
+            if (string.IsNullOrWhiteSpace(title) || string.IsNullOrWhiteSpace(author) ||
+                title == "Kitap İsmi" || author == "Yazar İsmi")
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurun.");
+                return;
+            }
+
+            Book newBook = new Book(title, author, year);
+            library.Add(newBook);
+            UpdateBookList();
+        }
+
+        private void UpdateBookList()
+        {
+            lstBooks.Items.Clear();
+            foreach (var book in library)
+            {
+                lstBooks.Items.Add($"{book.Title} - {book.Author} - {book.Year}");
+            }
+        }
+
+        private void txtTitle_Enter(object sender, EventArgs e)
+        {
+            if (txtTitle.Text == "Kitap İsmi")
+            {
+                txtTitle.Text = "";
+                txtTitle.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtTitle_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTitle.Text))
+            {
+                txtTitle.Text = "Kitap İsmi";
+                txtTitle.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtAuthor_Enter(object sender, EventArgs e)
+        {
+            if (txtAuthor.Text == "Yazar İsmi")
+            {
+                txtAuthor.Text = "";
+                txtAuthor.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtAuthor_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtAuthor.Text))
+            {
+                txtAuthor.Text = "Yazar İsmi";
+                txtAuthor.ForeColor = Color.Gray;
+            }
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public Book(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+    }
+}
